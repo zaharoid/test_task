ef1a10795ea1b2b7cfa57638451d064212d2973d
zaharoid <gololobov.zahar23@gmail.com>
2023-12-03 22:27:53 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/test_task">
         <App />
       </BrowserRouter>
     </PersistGate>

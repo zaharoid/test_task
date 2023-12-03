a66a1b952bb977cdeffae46513d9089851e42b46
zaharoid <gololobov.zahar23@gmail.com>
2023-12-03 22:30:49 +0100

    debag

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ import { store, persistor } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/test_task">
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  // {/* </React.StrictMode> */}
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/test_task">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
